3512b3e2c2b0ed19a94dfde049a788130953a2c5
DeepakfromSIT <124372302+DeepakfromSIT@users.noreply.github.com>
2024-01-09 20:18:43 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,9 @@
+const student = {
+  firstName: "Deepak",
+  lastName: "Pargaien",
+  age: 21,
+  marks: 80,
+};
+
+let para = document.getElementById("one");
+para.innerText = student.firstName + " " + student.lastName;
